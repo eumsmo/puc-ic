682a0fe19291ce7ac649d86656eb7239edc9c39e
Juan Vitor <juanriven10@gmail.com>
2024-10-16 00:29:32 -0300

    🧑‍💻 Maneiras de acesso
    Definindo duas maneiras diferentes de acessar o texto informado

--- IC/Assets/Scripts/GameManager.cs
+++ IC/Assets/Scripts/GameManager.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class GameManager : MonoBehaviour {
+    [DllImport("__Internal")]
+    private static extern string GetURLParams();
+
+
     public static GameManager instance;
-    public string texto = "teste.json";
+    public string texto = "info.json";
 
     ArtigoInfo artigoInfo;
 
@@ -17,22 +23,96 @@ public class GameManager : MonoBehaviour {
     }
 
     void Start() {
-        LoadTexto();
+        StartCoroutine(LoadTexto());
     }
 
-    public void LoadTexto() {
-        TextAsset file = Resources.Load<TextAsset>(texto);
-        if (file == null) {
-            Debug.LogError("Arquivo não encontrado");
-            return;
+    public IEnumerator LoadTexto() {
+        string url = GetURLInParams();
+
+        if (string.IsNullOrEmpty(url))
+            yield return StartCoroutine(LoadTextoLocal());
+        else
+            yield return StartCoroutine(LoadTextoWeb(url));
+    }
+
+    public string GetURLInParams() {
+        #if UNITY_WEBGL && !UNITY_EDITOR
+
+            string parametrosStr = GetURLParams();
+
+            if (string.IsNullOrEmpty(parametrosStr) || !parametrosStr.StartsWith("?")) {
+                return "";
+            }
+
+            // Remove o ? inicial
+            parametrosStr = parametrosStr.Substring(1);
+
+            // Separa cada parâmetro
+            string[] parametros = parametrosStr.Split('&');
+            string texto = "";
+
+            // Procura por pelo parâmetro 'url'
+            foreach (string parametro in parametros) {
+                if (parametro.StartsWith("url=")) {
+                    texto = parametro.Substring(4);
+                    break;
+                }
+            }
+
+            return texto;
+
+        #else
+
+            return "";
+
+        #endif
+    }
+
+    public IEnumerator LoadTextoWeb(string url) {
+        bool deuCerto = false;
+        string errorMessage = "Erro não especificado";
+
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(url)) {
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.result == UnityWebRequest.Result.ConnectionError) {
+                errorMessage = "Erro de rede: " + webRequest.error;
+            } else {
+                try {
+                    string json = webRequest.downloadHandler.text;
+                    artigoInfo = JsonUtility.FromJson<ArtigoInfo>(json);
+
+                    List<string> palavras = SepararPalavras(artigoInfo.resumo);
+                    deuCerto = true;
+
+                    UIController.instance.GerarPalavras(palavras);
+                } catch (System.Exception e) {
+                    errorMessage = "Erro de resposta: " + e.Message;
+                }
+            }
         }
 
+        if (!deuCerto) {
+            Debug.LogError(errorMessage);
+            yield return StartCoroutine(LoadTextoLocal());
+        }
+    }
 
-        string json = file.text;
-        artigoInfo = JsonUtility.FromJson<ArtigoInfo>(json);
+    public IEnumerator LoadTextoLocal() {
+        string rootPath = Application.dataPath;
+        string filePath = rootPath + "/info.json";
 
-        List<string> palavras = SepararPalavras(artigoInfo.resumo);
-        UIController.instance.GerarPalavras(palavras);
+        if (!System.IO.File.Exists(filePath)) {
+            Debug.LogError("Arquivo não encontrado: " + filePath);
+        } else {
+            string json = System.IO.File.ReadAllText(filePath);
+            artigoInfo = JsonUtility.FromJson<ArtigoInfo>(json);
+            
+            List<string> palavras = SepararPalavras(artigoInfo.resumo);
+            UIController.instance.GerarPalavras(palavras);
+        }
+
+        yield return null;
     }
 
     List<string> SepararPalavras(string texto) {
