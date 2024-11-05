1ca841710764fc0526009a2eb54814a452bc90dc
Juan Vitor <juanriven10@gmail.com>
2024-11-05 20:20:24 -0300

    🎨 Estilizando criação
    Agora tá mais bonitinho e iterativo, além de exibir o passo a passo tanto do automático quanto manual

--- Editor/src/publicar.js
+++ Editor/src/publicar.js
@@ -11,13 +11,25 @@ const criar_link_jogo = file_id => 'https://eumsmo.github.io/puc-ic/Build/?drive
 const file_name = 'jogo_artigo_info.json';
 
 // Elements
+const holder = document.querySelector('#holder');
 const tituloInput = document.querySelector('#titulo');
 const urlInput = document.querySelector('#url');
 const resumoInput = document.querySelector('#resumo');
 const indicador = document.querySelector('#indicadorProvisorio');
 
-const gerarBtn = document.querySelector('#gerarBtn');
-const gerarBtnGoogle = document.querySelector('#gerarBtnGoogle');
+const voltarBtns = document.querySelectorAll('.voltar');
+
+const irParaManualBtn = document.querySelector('#irManual');
+const baixarBtn = document.querySelector('#baixarArquivoBtn');
+const compartilhamentoLinkInput = document.querySelector('#compartilhamentoLink');
+const gerarLinkBtn = document.querySelector('#gerarLinkBtn');
+
+const irParaGoogleBtn = document.querySelector('#irGoogle');
+const googleProgress = document.querySelector('#googleProgress');
+const googleProgressText = document.querySelector('#googleProgressText');
+
+const linkHolder = document.querySelector('#linkEl');
+
 
 class Publicador {
     #auth_token = '';
@@ -43,6 +55,9 @@ class Publicador {
     async autenticar() {
         console.log('Autenticando...');
 
+        googleProgress.value = 0;
+        googleProgressText.innerHTML = 'Aguardando autenticação do usuário em outra janela...';
+
         let res = null;
 
         try {
@@ -60,12 +75,16 @@ class Publicador {
                 client.requestCode();
             });
         } catch (error) {
+            googleProgressText.innerHTML = 'Falha na autenticação';
+            setarEstado('valores');
             console.error(error);
         }
 
         if (res == null) return;
 
-        console.log(res);
+        googleProgress.value = 0.15;
+        googleProgressText.innerHTML = 'Gerando código de acesso...';
+
         let code = res.code;
 
         let token_res = await fetch(drive_token, {
@@ -121,16 +140,41 @@ class Publicador {
     }
 
     async salvarDocumentoDrive(documento, access_token = this.#auth_token) {
-        // Criar o documento
-        const uploadRes = await this.uplodarDocumentoDrive(documento, access_token);
+        googleProgress.value = 0.3; // 30%
+        googleProgressText.innerHTML = 'Dando upload do arquivo no drive...';
 
-        const file_id = uploadRes.id;
-        const permRes = await this.setarPermissoesDrive(file_id, access_token);
+        let file_id = null;
+        let deu_erro = false;
+
+        try {
+            // Criar o documento
+            const uploadRes = await this.uplodarDocumentoDrive(documento, access_token);
+
+            googleProgress.value = 0.7; // 70%
+            googleProgressText.innerHTML = 'Setando permissões...';
+
+            file_id = uploadRes.id;
+            const permRes = await this.setarPermissoesDrive(file_id, access_token);
+
+            googleProgress.value = 0.9; // 90%
+            googleProgressText.innerHTML = 'Gerando link...';
+            
+        } catch (error) {
+            googleProgressText.innerHTML = 'Falha ao salvar documento';
+            setarErro();
+            console.error(error);
+            deu_erro = true;
+        }
+
+        if (deu_erro) return;
         
         let link = criar_link_jogo(file_id);
         console.log(link);
 
-        indicador.innerHTML = `Link do jogo: <a href="${link}" target="_blank">${link}</a>`;
+        googleProgress.value = 1; // 90%
+        googleProgressText.innerHTML = 'Concluído';
+
+        setarLink(link);
     }
     
     gerarDocumento() {
@@ -145,6 +189,7 @@ class Publicador {
 
     async gerarGoogle() {
         await this.setup();
+        setarEstado('google');
 
         const file = this.gerarDocumento();
         let res = null;
@@ -152,12 +197,15 @@ class Publicador {
         try {
             res = await this.autenticar();
         } catch (error) {
+            googleProgressText.innerHTML = 'Falha na autenticação';
+            setarErro();
             console.error(error);
         }
 
         if (res == null) return;
-        
+
         let access_token = res.access_token;
+
         this.salvarDocumentoDrive(file, access_token);
     }
 
@@ -172,6 +220,29 @@ class Publicador {
         const file = this.gerarDocumento();
         this.salvarDocumentoPC(file);
     }
+
+    linkCompartilhamentoToGame(link) {
+        let parte = link.split('file/d/');
+        let id = parte[1].split('/')[0];
+        return criar_link_jogo(id);
+    }
+}
+
+estados = ['valores', 'manual', 'google', 'link', 'erro'];
+function setarEstado(estado) {
+    estados.forEach(e => holder.classList.remove(e));
+    holder.classList.add(estado);
+}
+
+function setarErro() {
+    holder.classList.add('erro');
+}
+
+function setarLink(link) {
+    linkHolder.innerHTML = link;
+    linkHolder.href = link;
+
+    setarEstado('link');
 }
 
 function receberAutenticacao(conteudo) {
@@ -180,5 +251,15 @@ function receberAutenticacao(conteudo) {
 
 const publicador = new Publicador();
 
-gerarBtnGoogle.addEventListener('click', () => publicador.gerarGoogle());
-gerarBtn.addEventListener('click', () => publicador.gerarManual());
+
+
+baixarBtn.addEventListener('click', () => publicador.gerarManual());
+gerarLinkBtn.addEventListener('click', () => {
+    let link = publicador.linkCompartilhamentoToGame(compartilhamentoLinkInput.value);
+    setarLink(link);
+});
+
+irParaManualBtn.addEventListener('click', () => setarEstado('manual'));
+irParaGoogleBtn.addEventListener('click', () => publicador.gerarGoogle());
+
+voltarBtns.forEach(btn => btn.addEventListener('click', () => setarEstado('valores')));
